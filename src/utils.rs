c1b49becd01c4f351e8e0727d669ce79838ae0df
Valeryi Savich <relrin78@gmail.com>
2019-12-13 00:05:20 +0200

    Implemented getting crate configuration

--- src/utils.rs
+++ src/utils.rs
@@ -1,1 +1,20 @@
+use std::process::Command;
 
+use rustc_serialize::json::Json;
+
+fn get_config() {
+    let path = get_config_path();
+}
+
+fn get_config_path() -> String {
+    let output = Command::new("cargo")
+         .arg("locate-project")
+         .output()
+         .expect("failed to run the `cargo locate-project` command.");
+
+    let json = match Json::from_str(&*output) {
+        Ok(j) => j,
+        Err(_) => panic!("Can't process response from the `cargo locate-project` command.")
+    };
+    json["root"].as_string().unwrap().to_string()
+}
