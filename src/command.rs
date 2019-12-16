68e516cc7592b151561a1a46ad5f424b8eae8bcc
Valeryi Savich <Relrin78@gmail.com>
2019-12-16 19:15:44 +0200

    Implemented method for getting a project directory

--- src/command.rs
+++ src/command.rs
@@ -1,10 +1,41 @@
-use cargo::util::CargoResult;
+use std::sync::{Arc, Mutex};
+
+use crate::error::Result;
+use crate::util::get_project_path;
+
+enum DependencyTypeEnum {
+    CratesIo,
+    Git,
+    Local,
+}
+
+struct Crate {
+    name: String,
+    version: String,
+    path: String,
+    dependency_type: DependencyTypeEnum,
+}
+
+struct CrateList {
+    all: Arc<Mutex<Vec<Crate>>>,
+    passed: Arc<Mutex<Vec<Crate>>>,
+    failed: Arc<Mutex<Vec<Crate>>>,
+}
 
 pub struct TestOptions {
     pub threads: u8,
     pub test_only: Vec<String>,
 }
 
-pub fn test_crates(options: &TestOptions) -> CargoResult<()> {
+pub fn test_crates(options: &TestOptions) -> Result<()> {
+    let path = get_project_path()?;
+    println!("{:?}", path);
+
+    let tested_crates = CrateList {
+        all: Arc::new(Mutex::new(Vec::new())),
+        passed: Arc::new(Mutex::new(Vec::new())),
+        failed: Arc::new(Mutex::new(Vec::new())),
+    };
+
     Ok(())
 }
--- src/util.rs
+++ src/util.rs
@@ -1,3 +1,4 @@
+use std::path::Path;
 use std::process::Command;
 
 use failure::ResultExt;
@@ -5,29 +6,32 @@ use rustc_serialize::json::Json;
 
 use crate::error::{ErrorKind, Result};
 
-pub fn get_config() -> Result<()> {
-    let path = get_config_path()?;
-    println!("{:?}", path);
-    Ok(())
-}
-
-pub fn get_config_path() -> Result<String> {
+pub fn get_project_path() -> Result<String> {
     let output = Command::new("cargo")
-         .arg("locate-project")
-         .output()
-         .context(ErrorKind::InvalidCommand {
-            description: String::from("can't execute the `cargo locate-project` command.")
-         })?;
+        .arg("locate-project")
+        .output()
+        .context(ErrorKind::InvalidCommand {
+            description: String::from("can't execute the `cargo locate-project` command."),
+        })?;
 
-    let response = String::from_utf8(output.stdout.clone())
-        .with_context(|err| ErrorKind::Utf8 {
+    let response =
+        String::from_utf8(output.stdout.clone()).with_context(|err| ErrorKind::Utf8 {
             value: output.stdout,
-            index: err.clone().utf8_error().valid_up_to()
+            index: err.clone().utf8_error().valid_up_to(),
         })?;
 
-    let json = Json::from_str(&response)
-        .context(ErrorKind::Other {
-           description: String::from("Can't parse a response from the `cargo locate-project` command.")
-        })?;
-    Ok(json["root"].as_string().unwrap_or("").to_string())
+    let json = Json::from_str(&response).context(ErrorKind::Other {
+        description: String::from(
+            "Can't parse a response from the `cargo locate-project` command.",
+        ),
+    })?;
+    let cargo_toml_path = json["root"].as_string().unwrap_or("").to_string();
+    let project_directory = Path::new(&cargo_toml_path)
+        .parent()
+        .expect("An attempt to get a parent for the root directory.")
+        .to_str()
+        .expect("Passed an invalid UTF-8 string.")
+        .to_string();
+
+    Ok(project_directory)
 }
