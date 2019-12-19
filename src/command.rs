079b38ff3c5f29cfa23f188abfe2b8f1660a6899
Valeryi Savich <Relrin78@gmail.com>
2019-12-19 10:28:04 +0200

    Implemented From trait for the Crate type

--- src/command.rs
+++ src/command.rs
@@ -1,32 +1,81 @@
 use std::path::Path;
+use std::str::FromStr;
 
 use cargo_lock::lockfile::Lockfile;
 use cargo_lock::package::Source;
 use failure::ResultExt;
+use lazy_static::lazy_static;
+use regex::Regex;
 
 use crate::error::{ErrorKind, Result};
 use crate::util::get_cargo_lock_path;
+use cargo_lock::Package;
 
+lazy_static! {
+    static ref URL_REGEX: Regex = Regex::new(
+        r"(?P<source_type>.+)\+(?P<url>[^\?\#]+)\??((tag=(?P<tag>[^#]+))?(#(?P<commit>.+)))?"
+    )
+    .unwrap();
+}
+
+#[derive(Debug, Clone)]
 enum DependencyTypeEnum {
     CratesIo,
     Git(SourceOptions),
-    GitLab(SourceOptions),
-    BitBucket(SourceOptions),
     Local,
 }
 
+#[derive(Debug, Clone)]
 struct SourceOptions {
-    branch: Option<String>,
     tag: Option<String>,
     commit: Option<String>,
 }
 
+#[derive(Debug, Clone)]
 struct Crate {
     name: String,
     path: String,
     dependency_type: DependencyTypeEnum,
 }
 
+impl From<Package> for Crate {
+    fn from(package: Package) -> Self {
+        let mut source = package
+            .source
+            .unwrap_or(Source::from_str("").unwrap())
+            .to_string();
+
+        let dependency_type = match URL_REGEX.captures(&source.clone()) {
+            Some(captures) => match &captures["source_type"] {
+                "git" => {
+                    source = match captures.name("url") {
+                        Some(value) => value.as_str().to_string(),
+                        _ => source,
+                    };
+                    let tag = match captures.name("tag") {
+                        Some(value) => Some(value.as_str().to_string()),
+                        _ => None,
+                    };
+                    let commit = match captures.name("commit") {
+                        Some(value) => Some(value.as_str().to_string()),
+                        _ => None,
+                    };
+                    DependencyTypeEnum::Git(SourceOptions { tag, commit })
+                }
+                _ => DependencyTypeEnum::CratesIo,
+            },
+            None => DependencyTypeEnum::Local,
+        };
+
+        Crate {
+            name: package.name.as_str().to_string(),
+            path: format!("{}", source),
+            dependency_type,
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
 struct CrateList {
     all: Vec<Crate>,
     passed: Vec<Crate>,
@@ -40,17 +89,13 @@ impl CrateList {
             ErrorKind::Io { reason: message }
         })?;
 
-        // TODO: Implement parsing source type
-        // TODO: Implement From<Package> for Crate ...
+        // TODO: Filter by defined dependencies in Cargo.toml
+        // TODO: Get sources from Cargo.toml dependencies section when used local dependency
         let used_crates = cargo_lock
             .packages
-            .iter()
-            .map(|package| Crate {
-                name: package.name.as_str().to_string(),
-                path: package.source.unwrap_or(Source(String::from(""))).unwrap(),
-                dependency_type: DependencyTypeEnum::CratesIo,
-            })
-            .collect();
+            .into_iter()
+            .map(|package| Crate::from(package.to_owned()))
+            .collect::<Vec<Crate>>();
 
         Ok(CrateList {
             all: used_crates,
@@ -75,6 +120,7 @@ impl CrateList {
     }
 }
 
+#[derive(Debug, Clone)]
 pub struct TestOptions {
     pub threads: u8,
     pub test_only: Vec<String>,
