b356964da1269523a8ef157f1b8a0b017be5b910
Valeryi Savich <Relrin78@gmail.com>
2020-01-13 16:51:58 +0200

    Added default implementation for running Cargo commands

--- src/runners/local.rs
+++ src/runners/local.rs
@@ -1,19 +1,27 @@
+use std::env::set_current_dir;
+
 use crate::command::Crate;
 use crate::error::Result;
 use crate::runners::traits::TestRunner;
 
-pub struct LocalDependencyTestRunner;
+pub struct LocalDependencyTestRunner {
+    sources_directory: String,
+}
 
 impl TestRunner for LocalDependencyTestRunner {
     fn new(dependency: &Crate) -> Self {
-        LocalDependencyTestRunner {}
+        LocalDependencyTestRunner {
+            sources_directory: dependency.get_path(),
+        }
     }
 
     fn setup(&self) -> Result<()> {
+        set_current_dir(self.sources_directory.clone())?;
         Ok(())
     }
 
     fn run_tests(&self) -> Result<()> {
+        self.run_cargo_command("test")?;
         Ok(())
     }
 
--- src/runners/traits.rs
+++ src/runners/traits.rs
@@ -1,11 +1,29 @@
+use std::process::{Command, Stdio};
+
 use crate::command::Crate;
-use crate::error::Result;
+use crate::error::{ErrorKind, Result};
+use failure::ResultExt;
 
 pub trait TestRunner {
     fn new(dependency: &Crate) -> Self
     where
         Self: Sized;
+
     fn setup(&self) -> Result<()>;
+
     fn run_tests(&self) -> Result<()>;
+
     fn teardown(&self) -> Result<()>;
+
+    fn run_cargo_command(&self, command: &str) -> Result<()> {
+        let mut process = Command::new("cargo")
+            .arg(command)
+            .stdout(Stdio::null())
+            .stderr(Stdio::null())
+            .spawn()?;
+        process.wait().with_context(|err| ErrorKind::Io {
+            reason: format!("{}", err),
+        })?;
+        Ok(())
+    }
 }
